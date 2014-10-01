658527bc4753ea8f71bc07b5b7c275cd91163091
andrew-luhring <andrew.luhring@gmail.com>
2014-10-01 15:56:25 -0500

    creates transform functions for posts into images using angular

--- assets/js/gallery/controllers/gallery.controller.js
+++ assets/js/gallery/controllers/gallery.controller.js
@@ -7,8 +7,35 @@ define([
   return ['$scope', 'DataService', '$sanitize', function($scope, DataService, $sanitize){
     var gallery = this;
     gallery.meta = $scope;
-
+    
     var data = DataService.data(this);
+    data.then(function(obj){
+      var strArr = [];
+
+      function returnUseableString(rex, str, arr ){
+
+      }
+
+
+      _.each(obj.postsArr, function(post){
+        var _post = post;
+        var str = post.markdown.toString();
+        var img = /'img':'[\/(\w)(\_\-)']*(\.png)'/ig;
+        var result = img.exec(str)
+        if(result){
+          var substr = result[0]
+          var path = substr.split(":")
+          path = path[1];
+          path = path.split("'");
+          path = path[1];
+          strArr.push(path);
+        }
+      })
+      window.strArr = strArr;
+      gallery.strArr = strArr;
+
+    });
+    
 
 
     function generatePostsForList(data){
@@ -30,12 +57,12 @@ define([
       gallery.listPosts = postHtmlArr;
 
     }
-    var list = DataService.list(this).
-      then(function(data){
-        generatePostsForList(data);
-      });
+//    var list = DataService.list(this).
+//      then(function(data){
+//        generatePostsForList(data);
+//      });
 
-
+    
     $scope.GalleryController = this;
     return $scope.CompanyController ;
   }];
--- assets/js/gallery/services/data.service.js
+++ assets/js/gallery/services/data.service.js
@@ -1,10 +1,59 @@
 define([
-  'lodash'
+    'lodash'
   , 'jquery'
+  , 'esapi'
   , 'transform'
-], function(_, _$){
+], function(_, _$, esapi){
+  
+  var obj = function(){
+    return {
+      esapi : function(){
+        return esapi;
+      }()
+    };
+
+  }();
+  //console.log (obj);
+  
+  var derp = function(){
+    return {
+      esapi : function(){
+        return esapi;
+      }()
+    };
+  }();
+
+  //console.log (derp);
+
+//  console.log (derp.test === obj.test);
+//
 
   return ['$http','$q', '$log' ,function($http, $q, $log){
+
+
+
+//    var img = /'img':'[\/(\w)(\_\-)']*(\.png)'/ig;
+//    var str = derp.toString()
+//    var result = img.exec(str)
+//    var substr = result[0]
+//    var path = substr.split(":")
+//    path = path[1];
+//    function transform(arr, container){
+//      var img = /'img':'[\/(\w)(\_\-)']*(\.png)'/ig;
+//
+//      _.each(arr, function(i){
+//        var md = i.markdown;
+//        var result = img.test(md);
+//
+//        if(result){
+//          var src = imgSrc.test(md);
+//        }
+//
+//      })
+//      return container;
+//    }
+
+
 
     this.data = function(obj){
         return $http.get("/nerd/content/images/2014/Sep/data.json").
@@ -54,7 +103,7 @@ define([
 
 
 
-//
+////
 //var img = /'img':'[\/(\w)(\_\-)']*(\.png)'/ig;
 //var str = derp.toString()
 //var result = img.exec(str)
