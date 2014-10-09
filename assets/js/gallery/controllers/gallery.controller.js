d4d3cfe3b12fcae178fb626861f322ee07b7b9bd
andrew-luhring <andrew.luhring@gmail.com>
2014-10-09 00:12:59 -0500

    adds comments to gallery controller, allows for the customization of the number of items in a row, adds rooGallery directive to links to begin the task of creating lightbox/shadowbox/whatever behavior

--- assets/js/gallery/controllers/gallery.controller.js
+++ assets/js/gallery/controllers/gallery.controller.js
@@ -29,25 +29,29 @@ define([
     }
   }
 
-
-  function wrapIntoRows(arrayOfItems){
+  /**
+   * Splits an array of items into smaller arrays within the item.
+   * @param   {Array}  - arrayOfItems
+   * @param   {Number} - numberOfRows (default is 4)
+   * @returns {Array}
+   */
+  function wrapIntoRows(arrayOfItems, numberOfRows){
     var items = arrayOfItems
+      , rows = numberOfRows || 4
       , wrappedDom = [];
 
-    function wrap(arr) {
-      if (typeof arr !== 'object' || arr instanceof Object !== true) { throw new TypeError(); }
-
-
+    function wrap(arr, num) {
       var surround
-        , notSurrounded;
-      console.log(arr);
-      if (arr.length > 4) {
-        surround = arr.slice(0, 4);
-        notSurrounded = arr.slice(4, arr.length);
+        , notSurrounded
+        , _num = num;
+
+      if (arr.length > _num) {
+        surround = arr.slice(0, _num);
+        notSurrounded = arr.slice(_num, arr.length);
         wrappedDom.push(surround);
-        wrap(notSurrounded);
+        wrap(notSurrounded, _num);
       } else {
-        var remainder = 4 - arr.length;
+        var remainder = _num - arr.length;
         surround = arr;
 
         for (var i = 0; i < remainder; i++) {
@@ -59,10 +63,10 @@ define([
       }
     }
 
-    wrap(items);
+    wrap(items, rows);
+
     return wrappedDom;
   }
-
 
   /**
    * this takes the promised data from DataService, formats it into a post, and attaches it to the gallery object(which was passed into Dataservice.data)
@@ -91,34 +95,10 @@ define([
           imgArr.push(final);
         }
       });
-
       gallery.rows = wrapIntoRows(imgArr);
-
-
-
       gallery.imgArr = imgArr;
     });
   }
-
-
-  function surroundIntoDom(arr){
-
-    $.each(arr, function(i){
-      var surrounderDiv = document.createElement('div');
-
-      $(surrounderDiv).addClass("surrounder");
-
-      $.each(arr[i], function(j){
-        $(arr[i][j]).appendTo(surrounderDiv);
-
-      });
-
-      $(surrounderDiv).appendTo("#galleryDiv");
-    });
-
-  }
-
-
   return ['$scope', 'DataService', '$sanitize', '$window', '$log', function($scope, DataService, $sanitize, $window, $log){
     var gallery = this
       , data = DataService.data(gallery);
--- assets/js/gallery/directives/gallery.directive.js
+++ assets/js/gallery/directives/gallery.directive.js
@@ -1,6 +1,26 @@
-define([], function() {
+define([
+  'lodash'
+], function(_) {
   "use strict";
-  return {
-
+  var types = ['illustration', 'website'];
+  function generateOverlay(attr){
+    var illustration = _.contains(attr, 'illustration')
+    , website = _.contains(attr, 'website');
+    if(illustration){
+    }
+  }
+  return function(){
+    return {
+      restrict: 'A'
+    , scope : {
+        rooGallery : '='
+      }
+    , link: function($scope, $elem, $attr){
+        $elem.click(function(e){
+          e.preventDefault();
+          generateOverlay($attr.rooGallery);
+        });
+      }
+    };
   };
 });
